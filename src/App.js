f4aa4cd2425a321bfacd6bd746648dc52ea2e4c6
junrui.deng <junrui.deng@accenture.com>
2020-04-23 23:49:06 +0800

    conplete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import Art from "./components/artCalculator";
 import Mathe from "./components/MathCalulator";
 import dataDemo from "./demoData";
@@ -6,159 +7,111 @@ import "./App.scss";
 
 import profile, { NameContext, EmailContext } from "./context/profieContext";
 
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.extras = {
-//       art: 0,
-//       mathe: 5
-//     };
-//     this.state = {
-//       _Average: null,
-//       _Subject: [],
-//       showArt: true,
-//       showMath: true,
-//       extras_Average: null,
-//       email: profile.email,
-//       name: profile.name,
-//       matcheExtra: this.extras.mathe
-//     };
-//     dataDemo.aerageChangeCb = _Average => {
-//       this.setState({
-//         _Average
-//       });
-//       this.handelExtras_Average();
-//     };
-//     dataDemo.subjectChangeCb = _Subject => {
-//       this.setState({
-//         _Subject
-//       });
-//     };
-//   }
-//   handelContextChange = e => {
-//     e.persist();
-//     this.setState({
-//       [e.target.name]: e.target.value
-//     });
-//   };
-//   handelExtras = e => {
-//     e.persist();
-//     this.extras[e.target.name] = parseInt(e.target.value, 10);
-//     if (e.target.name === "mathe") {
-//       this.setState({
-//         matcheExtra: this.extras.mathe
-//       });
-//     }
-//   };
-//   handelExtras_Average = () => {
-//     let extras_Average = null;
-//     let totle = 0;
-//     dataDemo._TotalSubject.forEach(e => {
-//       totle += e.score + parseInt(this.extras[e.code], 10);
-//     });
-//     extras_Average = (totle / dataDemo._TotalSubject.length).toFixed(2);
-//     this.setState({
-//       extras_Average
-//     });
-//   };
-//   toggle = e => {
-//     e.persist();
-//     const value = e.target.value;
-//     this.setState({
-//       [`show${value}`]: !this.state[`show${value}`]
-//     });
-//   };
-//   render() {
-//     console.log(this.context);
-//     return (
-//       <div className="average_warp">
-//         <p>
-//           email:<input
-//             name="email"
-//             value={this.state.email}
-//             onChange={this.handelContextChange}
-//             style={{ width: "200px" }}
-//           />
-//         </p>
-//         <p>
-//           name:{this.state.name}
-//         </p>
-//         <p>
-//           _Subject:{this.state._Subject.map(e =>
-//             <span key={e.code}>
-//               {e.code}:{e.score}/
-//             </span>
-//           )}
-//         </p>
-//         <p>
-//           _Average:{this.state._Average}
-//         </p>
-//         <p>
-//           Extras_Average: {this.state.extras_Average}
-//         </p>
-//         <div className="Calculator_warp">
-//           <div>
-//             <button onClick={this.toggle} value="Art">
-//               显示/影藏
-//             </button>
-//             <NameContext.Provider
-//               value={{
-//                 name: this.state.name,
-//                 changeName: this.handelContextChange
-//               }}
-//             >
-//               {this.state.showArt &&
-//                 <Art
-//                   title="请计算美术成绩"
-//                   onExtrasChange={this.handelExtras}
-//                   extrasElm={(extras, handelExtras) => {
-//                     this.extras.art = extras;
-//                     return (
-//                       <p>
-//                         extras:<input
-//                           type="number"
-//                           name="art"
-//                           value={extras}
-//                           onChange={handelExtras}
-//                         />
-//                       </p>
-//                     );
-//                   }}
-//                 />}
-//             </NameContext.Provider>
-//           </div>
-//           <div>
-//             <button onClick={this.toggle} value="Math">
-//               显示/影藏
-//             </button>
-//             <EmailContext.Provider value={this.state.email}>
-//               {this.state.showMath &&
-//                 <Mathe title="请计算数学成绩">
-//                   <p>
-//                     extras:<input
-//                       type="range"
-//                       min="-20"
-//                       max="20"
-//                       step="5"
-//                       name="mathe"
-//                       value={this.state.matcheExtra}
-//                       onChange={this.handelExtras}
-//                     />
-//                     <span>{this.state.matcheExtra}</span>
-//                   </p>
-//                 </Mathe>}
-//             </EmailContext.Provider>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 let extrasConf = {
   art: 0,
   mathe: 5
 };
+
+function Home(props) {
+  return <Link to='/art'>输入美术成绩</Link>
+}
+
+function noFound(props) {
+  return <Link to='/404'>404</Link>
+ }
+
+function Result(props) {
+  const { email, handelContextChange, name, _Subject = [], _Average, extras_Average } = props
+  return (<>
+      <p>
+        email:<input
+          name="email"
+          value={email}
+          onChange={handelContextChange}
+          style={{ width: "200px" }}
+        />
+      </p>
+      <p>
+        name:{name}
+      </p>
+      <p>
+        _Subject:{_Subject.map(e =>
+          <span key={e.code}>
+            {e.code}:{e.score}/
+          </span>
+        )}
+      </p>
+      <p>
+        _Average:{_Average}
+      </p>
+      <p>
+        Extras_Average: {extras_Average}
+      </p>
+    </>)
+
+}
+
+function ArtCalculator(props) {
+  const { name, handelContextChange, showArt, handelExtrasCb } = props
+  return <div>
+    <Link to="/math">
+      输入数学成绩
+    </Link>
+    <NameContext.Provider
+            value={{
+              name: name,
+              changeName: handelContextChange
+            }}
+          >
+            {showArt &&
+              <Art
+                title="请计算美术成绩"
+                onExtrasChange={handelExtrasCb}
+                extrasElm={(extras, handelExtras) => {
+                  extrasConf.art = extras;
+                  return (
+                    <p>
+                      extras:<input
+                        type="number"
+                        name="art"
+                        value={extras}
+                        onChange={handelExtras}
+                      />
+                    </p>
+                  );
+                }}
+              />}
+          </NameContext.Provider>
+  </div>
+}
+
+function MathCalculator(props) {
+  const { email, showMath, matcheExtra, handelExtrasCb } = props
+  return <div>
+    <Link to="/art">
+      输入美术成绩
+    </Link>
+    <EmailContext.Provider value={email}>
+            {showMath &&
+              <Mathe title="请计算数学成绩">
+                <p>
+                  extras:<input
+                    type="range"
+                    min="-20"
+                    max="20"
+                    step="5"
+                    name="mathe"
+                    value={matcheExtra}
+                    onChange={handelExtrasCb}
+                  />
+                  <span>{matcheExtra}</span>
+                </p>
+              </Mathe>}
+          </EmailContext.Provider>
+  </div>
+}
+
+
 function App() {
   let [_Average, set_Average] = useState("");
   dataDemo.aerageChangeCb = _Average => {
@@ -212,84 +165,20 @@ function App() {
   }
   return (
     <div className="average_warp">
-      <p>
-        email:<input
-          name="email"
-          value={email}
-          onChange={handelContextChange}
-          style={{ width: "200px" }}
-        />
-      </p>
-      <p>
-        name:{name}
-      </p>
-      <p>
-        _Subject:{_Subject.map(e =>
-          <span key={e.code}>
-            {e.code}:{e.score}/
-          </span>
-        )}
-      </p>
-      <p>
-        _Average:{_Average}
-      </p>
-      <p>
-        Extras_Average: {extras_Average}
-      </p>
-      <div className="Calculator_warp">
-        <div>
-          <button onClick={toggle} value="Art">
-            显示/影藏
-          </button>
-          <NameContext.Provider
-            value={{
-              name: name,
-              changeName: handelContextChange
-            }}
-          >
-            {showArt &&
-              <Art
-                title="请计算美术成绩"
-                onExtrasChange={handelExtrasCb}
-                extrasElm={(extras, handelExtras) => {
-                  extrasConf.art = extras;
-                  return (
-                    <p>
-                      extras:<input
-                        type="number"
-                        name="art"
-                        value={extras}
-                        onChange={handelExtras}
-                      />
-                    </p>
-                  );
-                }}
-              />}
-          </NameContext.Provider>
-        </div>
-        <div>
-          <button onClick={toggle} value="Math">
-            显示/影藏
-          </button>
-          <EmailContext.Provider value={email}>
-            {showMath &&
-              <Mathe title="请计算数学成绩">
-                <p>
-                  extras:<input
-                    type="range"
-                    min="-20"
-                    max="20"
-                    step="5"
-                    name="mathe"
-                    value={matcheExtra}
-                    onChange={handelExtrasCb}
-                  />
-                  <span>{matcheExtra}</span>
-                </p>
-              </Mathe>}
-          </EmailContext.Provider>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Switch>
+            <Route exact path="/home" component={Home} />
+            <Route path="/404" component={noFound} />
+            <Route path="/art" render={() => <ArtCalculator name={name} handelContextChange={handelContextChange}
+            showArt={showArt} handelExtrasCb={handelExtrasCb} />} />
+            <Route path="/math" render={() => <MathCalculator email={email} showMath={showMath} matcheExtra={matcheExtra}
+             handelExtrasCb={handelExtrasCb} />} />
+             <Route path="/result" children={({match, ...rest }) => <Result {...rest} email={email}
+             handelContextChange={handelContextChange} name={name} _Subject={_Subject} _Average={_Average}
+            extras_Average={extras_Average} />} />
+            <Route component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
